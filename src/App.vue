67c6cd6547b29d5556f6bb8689d8eaed0776c609
謝俊賢 <max.ghyun@gmail.com>
2021-11-11 20:44:31 +0800

    first comit
    新增vue3

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import GiftCheck from './components/GiftCheck.vue'
+</script>
+
+<template>
+  <GiftCheck />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #2c3e50;
+  height: 100vh;
+}
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/GiftCheck.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+
+
+const count = ref(0)
+</script>
+
+<template>
+  <section class="title">
+    <h1>生日大抽獎！來抽自己的生日禮物</h1>
+  </section>
+</template>
+
+<style scoped>
+h1 {
+  color: #fff;
+  margin: 0;
+}
+.title {
+  padding-top: 1rem;
+}
+</style>
